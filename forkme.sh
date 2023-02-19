a24f18972511dc90f143d1fb6a426f840c9b0a87
Marek Rychlik <rychlik@gmail.com>
2023-02-19 15:35:50 -0700

    *** empty log message ***

--- forkme.sh
+++ forkme.sh
@@ -2,6 +2,7 @@
 # EXAMPLE: Print 'Hello, ' and 'World!'
 # in random order w/o a random number generator.
 # HINT: We deliberately create a race condition.
+if !(( $# > 1 )) ;then ntimes=$1 ;else ntimes=10; fi
 function hello {
     echo -n "Hello, "
 }
@@ -9,7 +10,7 @@ function world {
     echo -n "World!"
 }
 dlay=1e-2  # Change to 5 to see processes
-for (( j=1; $j<10; j=$j+1 ))
+for (( j=1; $j<$ntimes; j=$j+1 ))
 do
     # Fork with '&'
     (sleep $dlay; hello) & (sleep $dlay; world)
