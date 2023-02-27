2c942b8c55b493ff6c29b809c92ed59f79a1b0dc
Marek Rychlik <rychlik@gmail.com>
2023-02-27 13:41:13 -0700

    *** empty log message ***

--- MATLAB/mandel.m
+++ MATLAB/mandel.m
@@ -1,4 +1,4 @@
-function count = mandel(x1,x2,y1,y2,gridSize,maxIterations)
+function count = mandel(x1,x2,y1,y2,gridSize,maxIterations,radius)
     x = linspace( x1, x2, gridSize(1) );
     y = linspace( y1, y2, gridSize(2) );
     [xGrid,yGrid] = meshgrid( x, y );
@@ -9,7 +9,7 @@ function count = mandel(x1,x2,y1,y2,gridSize,maxIterations)
     z = z0;
     for n = 0:maxIterations
         z = z.*z + z0;
-        inside = abs( z )<=2;
+        inside = abs( z )<=radius;
         count = count + inside;
     end
     count = log( count );
