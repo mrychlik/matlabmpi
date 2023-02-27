20bbd084f4669a9478c71bf2dc97c3d8e93300dc
Marek Rychlik <rychlik@arizona.edu>
2023-02-27 15:40:33 -0700

    *** empty log message ***

--- MATLAB/norace1.m
+++ MATLAB/norace1.m
@@ -1,9 +1,6 @@
 % FILE: norace1.m
 % This file demonstrates the best way for
-% workers to share a value. One flaw is that the value
-% is sent from every worker, and received multiple times.
-% Only one value will be trully received, the rest will be
-% discarded.
+% workers to share a value. 
 p=gcp('nocreate');
 if isempty(p)
     p = parpool('local', 8)
