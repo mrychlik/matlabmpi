26f73600c65f214a5f02c8580b2531fe917b0534
Marek Rychlik <rychlik@gmail.com>
2023-02-27 13:47:07 -0700

    *** empty log message ***

--- MATLAB/mandelbrotParallel.m
+++ MATLAB/mandelbrotParallel.m
@@ -23,6 +23,8 @@ ylim = [ 0.123640844894862,  0.123640851045266];
 
 % Setup
 x1 = xlim(1); x2=xlim(2); y1=ylim(1); y2=ylim(2);
+
+% Non-parallel calculation
 tic;
 count0 = mandel(x1, x2, y1, y2,gridSize,maxIterations,radius);
 toc;
