f7a04949600bd63f5b23c5f12dca5da376041f05
Marek Rychlik <rychlik@gmail.com>
2023-02-27 13:30:40 -0700

    *** empty log message ***

---
+++ MATLAB/mandel.m
@@ -0,0 +1,16 @@
+function count = mandel(x1,x2,y1,y2,gridSize,maxIterations)
+    x = linspace( x1, x2, gridSize(1) );
+    y = linspace( y1, y2, gridSize(2) );
+    [xGrid,yGrid] = meshgrid( x, y );
+    z0 = xGrid + 1i*yGrid;
+    count = ones( size(z0) );
+
+    % Calculate
+    z = z0;
+    for n = 0:maxIterations
+        z = z.*z + z0;
+        inside = abs( z )<=2;
+        count = count + inside;
+    end
+    count = log( count );
+end
