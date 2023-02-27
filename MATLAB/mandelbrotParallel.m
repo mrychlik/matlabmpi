6b68f09023cdc740e0436bf75460f31b6d3be763
Marek Rychlik <rychlik@gmail.com>
2023-02-27 13:45:03 -0700

    *** empty log message ***

--- MATLAB/mandelbrotParallel.m
+++ MATLAB/mandelbrotParallel.m
@@ -23,7 +23,9 @@ ylim = [ 0.123640844894862,  0.123640851045266];
 
 % Setup
 x1 = xlim(1); x2=xlim(2); y1=ylim(1); y2=ylim(2);
-%count = mandel(x1, x2, y1, y2,gridSize,maxIterations,radius);
+tic;
+count = mandel(x1, x2, y1, y2,gridSize,maxIterations,radius);
+toc;
 
 numSlices = p.NumWorkers*32;
 dx = (x2-x1)./numSlices;
